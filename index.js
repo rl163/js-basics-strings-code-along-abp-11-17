e8eabfbb76e067cf815b46c1e88c4b91fba55698
Ryan Lee <ryanlee91@gmail.com>
2017-11-09 03:31:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const currentUser = 'Ryan Lee';
 const welcomeMessage = 'Welcome to Flatbook, '+currentUser+"!";
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = "Welcome, ";
+const shortGreeting = 'Welcome, ${currentUser.slice(0,1)}!';
